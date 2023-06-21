958fca23534c68fb367476484cd530c35e8afc17
Liliia <derkach.lilia.iv@gmail.com>
2023-06-21 19:54:31 +0300

    Ready Calculate without Check by Illya

--- Task_4/ViewModel/CalculatorViewModel.cs
+++ Task_4/ViewModel/CalculatorViewModel.cs
@@ -15,6 +15,7 @@ namespace Task_4.ViewModel
         private Calculator _calculator;
         private string _example;
         private string _result;
+        private string _reset;
         public List<string> numericalExample = new List<string>();
 
         private string _numberFromButton;
@@ -24,38 +25,67 @@ namespace Task_4.ViewModel
 
         private int _firstOperandNum;
         private int _secondOperandNum;
-        private string _resultNum;
 
         public ICommand ShowExample { get; set; }
         public ICommand ShowResult { get; set; }
+        public ICommand ResetAll { get; set; }
 
         public CalculatorViewModel()
         {
             _calculator = new Calculator();
             ShowExample = new RelayCommand((parametr => CreateNumericExample(parametr)));
             ShowResult = new RelayCommand((parametr => CreateResult()));
+            ResetAll = new RelayCommand((paramr => Reset()));
         }
 
+        public void Reset()
+        {
+            ResetExample = null;
+            Example = null;
+            Result = null;
+            numericalExample = new List<string>();
+            _firstOperandStr = null;
+            _secondOperandStr = null;
+            _signOfExample = null;
+            _firstOperandNum = 0;
+            _secondOperandNum = 0;
+        }
         public void CreateNumericExample(object par)
         {
             _numberFromButton = par.ToString();
-
             numericalExample.Add(_numberFromButton);
-            //for (int i = 0; i > numericalExample.Count; i++)
-            //{
-            //    Example = numericalExample[i] + numericalExample[i-1];
-            //}
-            if (numericalExample.Count >= 3)
-            {
-                _firstOperandStr = numericalExample[0];
-                _signOfExample = numericalExample[1];
-                _secondOperandStr = numericalExample[2];
-            }
+            Example += _numberFromButton;
 
-            Example = $"{_firstOperandStr} {_signOfExample} {_secondOperandStr}";
+        }
+        public void CreateNumbers()
+        {
+            for (int i = 0; i < numericalExample.Count; i++)
+            {
+                bool isContinue = true;
+
+                if (_signOfExample != null)
+                {
+                    if (numericalExample[i] != "+" || numericalExample[i] != "-" || numericalExample[i] != "*" || numericalExample[i] != "/")
+                    { _secondOperandStr += numericalExample[i]; }
+                }
+                if (numericalExample[i] == "+" || numericalExample[i] == "-" || numericalExample[i] == "*" || numericalExample[i] == "/")
+                {
+                    _signOfExample = numericalExample[i];
+                }
+                while (isContinue == true && _signOfExample == null)
+                {
+                    if (numericalExample[i] != "+" || numericalExample[i] != "-" || numericalExample[i] != "*" || numericalExample[i] != "/")
+                    {
+                        _firstOperandStr += numericalExample[i];
+                        isContinue = false;
+                    }
+                }
+
+            }
         }
         public void CreateResult()
         {
+            CreateNumbers();
             switch (_signOfExample)
             {
                 case "+":
@@ -71,9 +101,18 @@ namespace Task_4.ViewModel
                     Result = "=" + Division();
                     break;
             }
-
-
         }
+
+        public string ResetExample
+        {
+            get { return _signOfExample; }
+            set
+            {
+                _reset = value;
+                OnPropertyChanged(nameof(Reset));
+            }
+        }
+
         public string Example
         {
             get { return _example; }
@@ -95,29 +134,29 @@ namespace Task_4.ViewModel
         }
         public string Addition()
         {
-            _firstOperandNum = Convert.ToInt32(numericalExample[0]);
-            _secondOperandNum = Convert.ToInt32(numericalExample[2]);
+            _firstOperandNum = Convert.ToInt32(_firstOperandStr);
+            _secondOperandNum = Convert.ToInt32(_secondOperandStr);
             _calculator.Result = _firstOperandNum + _secondOperandNum;
             return _calculator.Result.ToString();
         }
         public string Subtraction()
         {
-            _firstOperandNum = Convert.ToInt32(numericalExample[0]);
-            _secondOperandNum = Convert.ToInt32(numericalExample[2]);
+            _firstOperandNum = Convert.ToInt32(_firstOperandStr);
+            _secondOperandNum = Convert.ToInt32(_secondOperandStr);
             _calculator.Result = _firstOperandNum - _secondOperandNum;
             return _calculator.Result.ToString();
         }
         public string Division()
         {
-            _firstOperandNum = Convert.ToInt32(numericalExample[0]);
-            _secondOperandNum = Convert.ToInt32(numericalExample[2]);
+            _firstOperandNum = Convert.ToInt32(_firstOperandStr);
+            _secondOperandNum = Convert.ToInt32(_secondOperandStr);
             _calculator.Result = _firstOperandNum / _secondOperandNum;
             return _calculator.Result.ToString();
         }
         public string Multiplication()
         {
-            _firstOperandNum = Convert.ToInt32(numericalExample[0]);
-            _secondOperandNum = Convert.ToInt32(numericalExample[2]);
+            _firstOperandNum = Convert.ToInt32(_firstOperandStr);
+            _secondOperandNum = Convert.ToInt32(_secondOperandStr);
             _calculator.Result = _firstOperandNum * _secondOperandNum;
             return _calculator.Result.ToString();
         }
