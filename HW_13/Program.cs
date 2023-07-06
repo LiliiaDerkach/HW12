fd2815bdff5c0e95e0df657602b99cacf782a25d
Liliia <derkach.lilia.iv@gmail.com>
2023-07-06 21:44:20 +0300

    Trying to do several lines by for

--- HW_13/Program.cs
+++ HW_13/Program.cs
@@ -16,42 +16,66 @@ namespace HW_13
             int lengthOfLine = 0;
             int widthOfWindow = Console.WindowWidth;
             int heightOfWindow = Console.WindowHeight;
-            int numberOfLine = random.Next(0, widthOfWindow);
+            int numberOfColoumn = random.Next(0, widthOfWindow);
             lengthOfLine = random.Next(0, 10);
             string line = null;
+            int numberOfLine = 0;
 
-            //for (int i = 0; i < heightOfWindow; i++)
+            //for (int i = 0; i < lengthOfLine; i++)
             //{
-            //Console.Clear();
-            //    Console.SetCursorPosition(numberOfLine, i);
-            //    Console.ForegroundColor = ConsoleColor.DarkGreen;
-            //    if (i == lengthOfLine - 1)
-
-            //        Console.ForegroundColor = ConsoleColor.White;
-            //    if (i == lengthOfLine - 2)
-            //        Console.ForegroundColor = ConsoleColor.Yellow;
-            //    line = letters[random.Next(letters.Length)].ToString();
+            //    line += letters[random.Next(letters.Length)].ToString();
             //    Console.WriteLine(line);
             //    Thread.Sleep(1000);
             //}
-
             for (int i = 0; i < heightOfWindow; i++)
             {
                 Console.Clear();
-                Console.SetCursorPosition(numberOfLine, i);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(letters[random.Next(letters.Length)]);
-                Console.SetCursorPosition(numberOfLine, i+1);
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine(letters[random.Next(letters.Length)]);
-                Console.SetCursorPosition(numberOfLine, i + 2);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(letters[random.Next(letters.Length)]);
-                Console.SetCursorPosition(numberOfLine, i + 3);
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine(letters[random.Next(letters.Length)]);
-                Thread.Sleep(1000);
+                if (i < lengthOfLine)
+                {
+                    line = null;
+                    for (int a = 0; a < i; a++)
+                    {
+                        Console.SetCursorPosition(numberOfColoumn, a);
+                        line += letters[random.Next(letters.Length)].ToString();
+                        Console.WriteLine(line);
+                        Thread.Sleep(500);
+                    }
+                }
+                if (i >= lengthOfLine)
+                {
+                    line = null;
+                    for (int a = 0; a < lengthOfLine; a++)
+                    {
+                        if (numberOfLine < heightOfWindow)
+                        {
+                            Console.SetCursorPosition(numberOfColoumn, numberOfLine);
+                            numberOfLine += 1;
+                            line += letters[random.Next(letters.Length)].ToString();
+                            Console.WriteLine(line);
+                            Thread.Sleep(500);
+                        }
+                    }
+                }
+
             }
+
+            //for (int i = 0; i < heightOfWindow; i++)
+            //{
+            //    Console.Clear();
+            //    Console.SetCursorPosition(numberOfColoumn, i);
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine(letters[random.Next(letters.Length)]);
+            //    Console.SetCursorPosition(numberOfColoumn, i + 1);
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    Console.WriteLine(letters[random.Next(letters.Length)]);
+            //    Console.SetCursorPosition(numberOfColoumn, i + 2);
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine(letters[random.Next(letters.Length)]);
+            //    Console.SetCursorPosition(numberOfColoumn, i + 3);
+            //    Console.ForegroundColor = ConsoleColor.Blue;
+            //    Console.WriteLine(letters[random.Next(letters.Length)]);
+            //    Thread.Sleep(1000);
+            //}
             //}
         }
         static void Main(string[] args)
@@ -60,6 +84,8 @@ namespace HW_13
             Thread line2 = new Thread(Stroka);
             Thread line3 = new Thread(Stroka);
             line1.Start();
+            //line2.Start();
+            //line3.Start();
 
             Console.ReadKey();
         }
